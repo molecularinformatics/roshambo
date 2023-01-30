4d5481fc7fd81fa49b1a7ba8ec4f5c6752a1e4bc
ratwi <rasha.atwi@biogen.com>
2023-01-30 16:07:17 -0500

    Create Grid

--- grid.py
+++ grid.py
@@ -1,22 +1,37 @@
+import numpy as np
+
+
 class Grid:
-    def __init__(self, mol, res, margin):
+    def __init__(self, mol, res=0.4, margin=0.4):
         self.mol = mol
         self.res = res
         self.margin = margin
+        self.extent = None
+        self.converted_grid = None
 
     def get_bounding_box(self):
-        lb = [float("inf"), float("inf"), float("inf")]
-        ub = [float("-inf"), float("-inf"), float("-inf")]
-        for i in range(self.mol.natoms):
-            lb[0] = min(lb[0], self.mol.atoms[i].x)
-            lb[1] = min(lb[1], self.mol.atoms[i].y)
-            lb[2] = min(lb[2], self.mol.atoms[i].z)
-            ub[0] = max(ub[0], self.mol.atoms[i].x)
-            ub[1] = max(ub[1], self.mol.atoms[i].y)
-            ub[2] = max(ub[2], self.mol.atoms[i].z)
-        lb = [x - self.margin for x in lb]
-        ub = [x + self.margin for x in ub]
+        coords_radii = self.mol.get_atomic_coordinates_and_radii()
+        coords = coords_radii[:, :3]
+        radii = coords_radii[:, 3]
+        lb = coords - radii[:, None]
+        ub = coords + radii[:, None]
+        lb = lb.min(axis=0)
+        ub = ub.max(axis=0)
+        lb -= self.margin
+        ub += self.margin
         return lb, ub
 
-    def get_host_grid(self):
-        pass
+    def convert_grid_to_real(self, lb, coords):
+        grid_coords = np.array([x + 0.5 for x in coords])
+        return lb + grid_coords * self.res
+
+    def create_grid(self):
+        lb, ub = self.get_bounding_box()
+        dims = ub - lb
+        self.extent = np.ceil(dims / self.res).astype(int)
+        self.converted_grid = np.array(
+            [
+                self.convert_grid_to_real(lb, coords)
+                for coords in np.ndindex(self.extent[0], self.extent[1], self.extent[2])
+            ]
+        )
