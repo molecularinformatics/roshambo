d4c874be87d2ee0e85afa886193e5654eadc570e
ratwi <rasha.atwi@biogen.com>
2023-03-06 11:51:17 -0500

    Convert rdkit molecule to CUDA molecule

--- paper/inputFileReader.cpp
+++ paper/inputFileReader.cpp
@@ -110,6 +110,115 @@ float3 atomset_centroid(OBMol& mol,const set<int>& atoms) {
     return cen;
 }
 
+CUDAmol rdMolToCUDAmol(RDKit::ROMol* rdmol)
+{
+    CUDAmol mol;
+    if (!rdmol) throw std::invalid_argument("Invalid molecule object");
+    #ifdef IGNORE_HYDROGENS
+    const unsigned int natoms = rdmol->getNumHeavyAtoms();
+    #else
+    const unsigned int natoms = rdmol->getNumAtoms();
+    #endif
+    mol.natoms = natoms;
+    mol.atoms = new float4[mol.natoms];
+    const auto &conf = rdmol->getConformer();
+    unsigned int idx = 0;
+    const PeriodicTable *table = PeriodicTable::getTable();
+    for (unsigned int i = 0; i < natoms; ++i) {
+        const auto& atom = rdmol->getAtomWithIdx(i);
+        const auto& coords = conf.getAtomPos(i);
+        mol.atoms[idx].x = coords.x;
+        mol.atoms[idx].y = coords.y;
+        mol.atoms[idx].z = coords.z;
+        #ifdef ALL_CARBON_RADII
+        mol.atoms[idx].w = 1.70f;
+        #else
+        mol.atoms[idx].w = table->getRvdw(atom->getAtomicNum());
+        #endif
+        ++idx;
+    }
+    return mol;
+}
+
+dCUDAmol rdMolTodCUDAmol(RDKit::ROMol* rdmol)
+{
+    dCUDAmol mol;
+    if (!rdmol) throw std::invalid_argument("Invalid molecule object");
+    #ifdef IGNORE_HYDROGENS
+    const unsigned int natoms = rdmol->getNumHeavyAtoms();
+    #else
+    const unsigned int natoms = rdmol->getNumAtoms();
+    #endif
+    mol.natoms = natoms;
+    mol.x = new float[natoms];
+    mol.y = new float[natoms];
+    mol.z = new float[natoms];
+    mol.a = new float[natoms];
+
+    const float partialalpha = 2.41798793102f;
+    const auto &conf = rdmol->getConformer();
+    unsigned int idx = 0;
+    const PeriodicTable *table = PeriodicTable::getTable();
+    for (unsigned int i = 0; i < natoms; ++i) {
+        const auto& atom = rdmol->getAtomWithIdx(i);
+        const auto& coords = conf.getAtomPos(i);
+        #ifdef IGNORE_HYDROGENS
+        if (table->getRvdw(atom->getAtomicNum()) == 1) continue;
+        #endif
+        mol.x[idx] = coords.x;
+        mol.y[idx] = coords.y;
+        mol.z[idx] = coords.z;
+        #ifdef ALL_CARBON_RADII
+        float rad = 1.70f;
+        #else
+        float rad = table->getRvdw(atom->getAtomicNum());
+        #endif
+        mol.a[idx] = partialalpha / (rad * rad);
+        ++idx;
+    }
+    return mol;
+}
+
+float3 atomset_centroid_rdkit(RDKit::ROMol* rdmol, const std::set<int>& atoms) {
+    float3 cen;
+    cen.x = cen.y = cen.z = 0.0f;
+    const auto &conf = rdmol->getConformer();
+
+    for (const auto& idx : atoms) {
+        const auto& coords = conf.getAtomPos(idx);
+        cen.x += coords.x;
+        cen.y += coords.y;
+        cen.z += coords.z;
+    }
+    const int num_atoms = static_cast<float>(atoms.size());
+    cen.x /= num_atoms;
+    cen.y /= num_atoms;
+    cen.z /= num_atoms;
+
+    return cen;
+}
+
+list<set<int>> find_ring_systems_rdkit(RDKit::ROMol* rdmol, bool includeSpiro=false) {
+    RingInfo *ring_info = rdmol->getRingInfo();
+    list<set<int>> systems;
+    for (const auto& ring : ring_info->atomRings()) {
+        set<int> ringAts(ring.begin(), ring.end());
+        list<set<int>> nSystems;
+        for (const auto& system : systems) {
+            int nInCommon = count_if(system.begin(), system.end(),
+                                     [&](int i){ return ringAts.count(i) > 0; });
+            if (nInCommon && (includeSpiro || nInCommon > 1)) {
+                ringAts.insert(system.begin(), system.end());
+            } else {
+                nSystems.push_back(system);
+            }
+        }
+        nSystems.push_back(ringAts);
+        systems = nSystems;
+    }
+    return systems;
+}
+
 extern "C" void molFromFile(string filename,list<CUDAmol>& cmols,list<dCUDAmol>& dcmols,list<list<float3> >& ring_centroids) {
     OBConversion conv;
     ifstream ifs(filename.c_str(),ifstream::in|ifstream::binary);
