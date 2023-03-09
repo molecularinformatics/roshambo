2166700a942f1758d6b5d5236a72535fe707cc5c
ratwi <rasha.atwi@biogen.com>
2023-03-09 11:36:18 -0500

    Return overlap volumes instead of tanimotos

--- pypaper/volume.py
+++ pypaper/volume.py
@@ -30,8 +30,46 @@ def calculate_analytic_overlap_volume(ref_mol, fit_mol):
     return overlap
 
 
-def calculate_tanimoto_analytic(ref_mol, fit_mol, ref_overlap):
+def calculate_tanimoto_analytic(ref_mol, fit_mol):
     fit_overlap = calculate_analytic_overlap_volume(fit_mol, fit_mol)
     ref_fit_overlap = calculate_analytic_overlap_volume(ref_mol, fit_mol)
-    tanimoto = ref_fit_overlap / (ref_overlap + fit_overlap - ref_fit_overlap)
-    return tanimoto
+    # tanimoto = ref_fit_overlap / (ref_overlap + fit_overlap - ref_fit_overlap)
+    return fit_overlap, ref_fit_overlap
+
+
+def rho(atoms, gcs):
+    rt22 = 2.82842712475
+    alphas = -PARTIAL_ALPHA / (atoms[:, 0, 3] ** 2)
+    diffs = gcs[:, np.newaxis, :] - atoms[:, 0, :3]
+    r2s = np.sum(diffs * diffs, axis=-1)
+    rhos = rt22 * np.exp(alphas[np.newaxis, :] * r2s)
+    return rhos
+
+
+def calculate_gaussian_overlap_volume(ref_mol, fit_mol, grid):
+    gcs = grid.converted_grid
+    ref_mol_coords_radii = ref_mol.get_atomic_coordinates_and_radii()
+    fit_mol_coords_radii = fit_mol.get_atomic_coordinates_and_radii()
+
+    rho_ref = rho(ref_mol_coords_radii[:, np.newaxis], gcs)
+    ref_grid = 1 - np.prod(1 - rho_ref, axis=1)
+
+    rho_fit = rho(fit_mol_coords_radii[:, np.newaxis], gcs)
+    fit_grid = 1 - np.prod(1 - rho_fit, axis=1)
+    volume = np.sum(ref_grid * fit_grid) * grid.res ** 3
+    return volume
+
+
+def calculate_tanimoto_gaussian(
+    fit_mol, res, margin, ref_grid, ref_mol
+):
+    fit_grid = Grid(fit_mol, res=res, margin=margin)
+    fit_grid.create_grid()
+    fit_overlap = calculate_gaussian_overlap_volume(fit_mol, fit_mol, fit_grid)
+
+    ref_fit_overlap = calculate_gaussian_overlap_volume(ref_mol, fit_mol,
+        ref_grid
+        if np.prod(ref_grid.extent) < np.prod(fit_grid.extent)
+        else fit_grid
+    )
+    return fit_overlap, ref_fit_overlap
