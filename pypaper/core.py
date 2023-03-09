1708e7e79e4a29d31a360c30803a8dc55d59a1d7
ratwi <rasha.atwi@biogen.com>
2023-03-09 11:28:04 -0500

    Run paper using cython instead of subprocess

--- pypaper/core.py
+++ pypaper/core.py
@@ -68,47 +68,51 @@ class GetSimilarityScores:
         mol.write_molfile(file)
         return mol
 
-    def run_paper(self, paper_cmd=None, gpu_id=0, cleanup=True):
-        run_file = f"{self.working_dir}/runfile"
-        with open(run_file, "w") as f:
-            for file in [self.ref_file] + self.dataset_files:
-                f.write(file + "\n")
+    # def run_paper(self, paper_cmd=None, gpu_id=0, cleanup=True):
+    #     run_file = f"{self.working_dir}/runfile"
+    #     with open(run_file, "w") as f:
+    #         for file in [self.ref_file] + self.dataset_files:
+    #             f.write(file + "\n")
+    #
+    #     # TODO: add mode and arguments that can be specified to paper
+    #     # TODO: include a case where the run_file is provided as input
+    #     if not paper_cmd:
+    #         cfg = ConfigParser()
+    #         cfg.read(os.environ.get("CONFIG_FILE_PATH", "../config/config.ini"))
+    #         cmd = cfg["RunPAPER"]["paper_cmd"]
+    #         paper_cmd = cmd.replace("$gpu_id$", str(gpu_id)).replace(
+    #             "$run_file$", run_file
+    #         )
+    #
+    #     st = timer()
+    #     return_code = subprocess.run(
+    #         paper_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    #     )
+    #     run_time = timer() - st
+    #     print(f"Run time: {run_time}")
+    #
+    #     output = return_code.stdout.decode()
+    #     output_strings = output.split("[[")
+    #     output_strings = [i.replace("]]", "") for i in output_strings]
+    #     output_strings = [i.replace("\n", " ") for i in output_strings]
+    #     output_strings = [i.strip() for i in output_strings if i]
+    #
+    #     # convert each string into a numpy array
+    #     output_arrays = [
+    #         np.fromstring(output_string, dtype=float, sep=" ")
+    #         for output_string in output_strings
+    #     ]
+    #     self.transformation_arrays = [
+    #         np.reshape(output_array, (4, 4)) for output_array in output_arrays
+    #     ]
+    #
+    #     if cleanup:
+    #         print("Cleaning up...")
+    #         os.remove(f"{self.working_dir}/runfile")
 
-        # TODO: add mode and arguments that can be specified to paper
-        # TODO: include a case where the run_file is provided as input
-        if not paper_cmd:
-            cfg = ConfigParser()
-            cfg.read(os.environ.get("CONFIG_FILE_PATH", "../config/config.ini"))
-            cmd = cfg["RunPAPER"]["paper_cmd"]
-            paper_cmd = cmd.replace("$gpu_id$", str(gpu_id)).replace(
-                "$run_file$", run_file
-            )
-
-        st = timer()
-        return_code = subprocess.run(
-            paper_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-        run_time = timer() - st
-        print(f"Run time: {run_time}")
-
-        output = return_code.stdout.decode()
-        output_strings = output.split("[[")
-        output_strings = [i.replace("]]", "") for i in output_strings]
-        output_strings = [i.replace("\n", " ") for i in output_strings]
-        output_strings = [i.strip() for i in output_strings if i]
-
-        # convert each string into a numpy array
-        output_arrays = [
-            np.fromstring(output_string, dtype=float, sep=" ")
-            for output_string in output_strings
-        ]
-        self.transformation_arrays = [
-            np.reshape(output_array, (4, 4)) for output_array in output_arrays
-        ]
-
-        if cleanup:
-            print("Cleaning up...")
-            os.remove(f"{self.working_dir}/runfile")
+    def run_paper(self, gpu_id=0):
+        molecules = [self.ref_mol] + self.dataset_mols
+        self.transformation_arrays = cpaper(gpu_id, molecules)
 
     def convert_transformation_arrays(self):
         # Extract rotation matrix and translation vector from transformation matrix
