b32817f043149358d6d57b19b6dcc00047f2825f
ratwi <rasha.atwi@biogen.com>
2023-03-23 10:45:40 -0400

    Reorder analytic functions

--- pypaper/analytic_overlap.py
+++ pypaper/analytic_overlap.py
@@ -52,86 +52,6 @@ def calc_single_overlap(atom_inds, alpha_dict, cross_alpha_distance_dict):
     return p * k_exp * (PI / alpha) ** 1.5
 
 
-def calc_analytic_overlap_vol_iterative(ref_mol, fit_mol, n=6):
-    ref_mol_coords_radii = ref_mol.get_atomic_coordinates_and_radii()
-    fit_mol_coords_radii = fit_mol.get_atomic_coordinates_and_radii()
-    ref_len = len(ref_mol_coords_radii)
-    all_radii = np.concatenate([ref_mol_coords_radii, fit_mol_coords_radii])
-    alpha_dict = {i: KAPPA / j[3] ** 2 for i, j in enumerate(all_radii)}
-    cross_alpha_dict = {
-        (i, j): alpha_dict[i] * alpha_dict[j]
-        for i in range(len(all_radii))
-        for j in range(len(all_radii))
-    }
-    cross_alpha_distance_dict = {
-        (i, j): cross_alpha_dict[(i, j)]
-        * np.sum((all_radii[i][:3] - all_radii[j][:3]) ** 2)
-        for i in range(len(all_radii))
-        for j in range(len(all_radii))
-    }
-
-    full_len = len(all_radii)
-    overlap = 0
-    tot_time = 0
-    for fit_atom_ind in range(len(fit_mol_coords_radii)):
-        remove_2 = {len(ref_mol_coords_radii) + k for k in range(fit_atom_ind + 1)}
-        for ref_atom_ind in range(len(ref_mol_coords_radii)):
-            atom_inds = [fit_atom_ind + ref_len, ref_atom_ind]
-            overlap += calc_single_overlap(
-                atom_inds, alpha_dict, cross_alpha_distance_dict
-            )
-            if n == 2:
-                continue
-            remove_3 = remove_2 | {ii for ii in range(ref_atom_ind + 1)}
-            for third_order_ind in range(full_len):
-                if third_order_ind in remove_3:
-                    continue
-                third_order_atom_inds = atom_inds + [third_order_ind]
-                overlap -= calc_single_overlap(
-                    third_order_atom_inds, alpha_dict, cross_alpha_distance_dict
-                )
-                if n == 3:
-                    continue
-                remove_4 = remove_3 | {ii for ii in range(third_order_ind + 1)}
-                for fourth_order_ind in range(full_len):
-                    if fourth_order_ind in remove_4:
-                        continue
-                    fourth_order_atom_inds = third_order_atom_inds + [fourth_order_ind]
-                    overlap += calc_single_overlap(
-                        fourth_order_atom_inds, alpha_dict, cross_alpha_distance_dict
-                    )
-                    if n == 4:
-                        continue
-                    remove_5 = remove_4 | {ii for ii in range(fourth_order_ind + 1)}
-                    for fifth_order_ind in range(full_len):
-                        if fifth_order_ind in remove_5:
-                            continue
-                        fifth_order_atom_inds = fourth_order_atom_inds + [
-                            fifth_order_ind
-                        ]
-                        st = time.time()
-                        overlap -= calc_single_overlap(
-                            fifth_order_atom_inds, alpha_dict, cross_alpha_distance_dict
-                        )
-                        tot_time += time.time() - st
-                        if n == 5:
-                            continue
-                        remove_6 = remove_5 | {ii for ii in range(fifth_order_ind + 1)}
-                        for sixth_order_ind in range(full_len):
-                            if sixth_order_ind in remove_6:
-                                continue
-                            sixth_order_atom_inds = fifth_order_atom_inds + [
-                                sixth_order_ind
-                            ]
-                            overlap += calc_single_overlap(
-                                sixth_order_atom_inds,
-                                alpha_dict,
-                                cross_alpha_distance_dict,
-                            )
-    print("TOT TIME", tot_time)
-    return overlap
-
-
 def _calc_overlap(
     atom_inds,
     rem_ind,
@@ -264,3 +184,83 @@ def calc_multi_analytic_overlap_vol_recursive(
         use_carbon_radii=use_carbon_radii,
     )
     return fit_overlap, ref_fit_overlap
+
+
+def calc_analytic_overlap_vol_iterative(ref_mol, fit_mol, n=6):
+    ref_mol_coords_radii = ref_mol.get_atomic_coordinates_and_radii()
+    fit_mol_coords_radii = fit_mol.get_atomic_coordinates_and_radii()
+    ref_len = len(ref_mol_coords_radii)
+    all_radii = np.concatenate([ref_mol_coords_radii, fit_mol_coords_radii])
+    alpha_dict = {i: KAPPA / j[3] ** 2 for i, j in enumerate(all_radii)}
+    cross_alpha_dict = {
+        (i, j): alpha_dict[i] * alpha_dict[j]
+        for i in range(len(all_radii))
+        for j in range(len(all_radii))
+    }
+    cross_alpha_distance_dict = {
+        (i, j): cross_alpha_dict[(i, j)]
+        * np.sum((all_radii[i][:3] - all_radii[j][:3]) ** 2)
+        for i in range(len(all_radii))
+        for j in range(len(all_radii))
+    }
+
+    full_len = len(all_radii)
+    overlap = 0
+    tot_time = 0
+    for fit_atom_ind in range(len(fit_mol_coords_radii)):
+        remove_2 = {len(ref_mol_coords_radii) + k for k in range(fit_atom_ind + 1)}
+        for ref_atom_ind in range(len(ref_mol_coords_radii)):
+            atom_inds = [fit_atom_ind + ref_len, ref_atom_ind]
+            overlap += calc_single_overlap(
+                atom_inds, alpha_dict, cross_alpha_distance_dict
+            )
+            if n == 2:
+                continue
+            remove_3 = remove_2 | {ii for ii in range(ref_atom_ind + 1)}
+            for third_order_ind in range(full_len):
+                if third_order_ind in remove_3:
+                    continue
+                third_order_atom_inds = atom_inds + [third_order_ind]
+                overlap -= calc_single_overlap(
+                    third_order_atom_inds, alpha_dict, cross_alpha_distance_dict
+                )
+                if n == 3:
+                    continue
+                remove_4 = remove_3 | {ii for ii in range(third_order_ind + 1)}
+                for fourth_order_ind in range(full_len):
+                    if fourth_order_ind in remove_4:
+                        continue
+                    fourth_order_atom_inds = third_order_atom_inds + [fourth_order_ind]
+                    overlap += calc_single_overlap(
+                        fourth_order_atom_inds, alpha_dict, cross_alpha_distance_dict
+                    )
+                    if n == 4:
+                        continue
+                    remove_5 = remove_4 | {ii for ii in range(fourth_order_ind + 1)}
+                    for fifth_order_ind in range(full_len):
+                        if fifth_order_ind in remove_5:
+                            continue
+                        fifth_order_atom_inds = fourth_order_atom_inds + [
+                            fifth_order_ind
+                        ]
+                        st = time.time()
+                        overlap -= calc_single_overlap(
+                            fifth_order_atom_inds, alpha_dict, cross_alpha_distance_dict
+                        )
+                        tot_time += time.time() - st
+                        if n == 5:
+                            continue
+                        remove_6 = remove_5 | {ii for ii in range(fifth_order_ind + 1)}
+                        for sixth_order_ind in range(full_len):
+                            if sixth_order_ind in remove_6:
+                                continue
+                            sixth_order_atom_inds = fifth_order_atom_inds + [
+                                sixth_order_ind
+                            ]
+                            overlap += calc_single_overlap(
+                                sixth_order_atom_inds,
+                                alpha_dict,
+                                cross_alpha_distance_dict,
+                            )
+    print("TOT TIME", tot_time)
+    return overlap
