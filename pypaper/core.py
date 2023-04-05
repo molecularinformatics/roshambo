3b5b6b970b2e4b6f2a17dc73d7466c122fed7251
ratwi <rasha.atwi@biogen.com>
2023-04-05 13:53:06 -0400

    Cleanups

--- pypaper/core.py
+++ pypaper/core.py
@@ -23,20 +23,17 @@ from pypaper.grid_overlap import (
     calc_multi_gaussian_overlap_vol,
 )
 from pypaper.analytic_overlap import (
-    calc_analytic_overlap_vol,
-    calc_multi_analytic_overlap_vol,
     calc_analytic_overlap_vol_recursive,
     calc_multi_analytic_overlap_vol_recursive,
 )
 from pypaper.scores import calc_tanimoto, calc_tversky
-from pypaper.pharmacophore import color_tanimoto
 from pypaper.pharmacophore import (
     calc_pharmacophore,
     calc_pharm_overlap,
     calc_multi_pharm_overlap,
 )
 from pypaper.structure import Molecule
-from pypaper.utilities import split_sdf_file, prepare_mols
+from pypaper.utilities import prepare_mols
 
 
 class GetSimilarityScores:
@@ -44,7 +41,6 @@ class GetSimilarityScores:
         self,
         ref_file,
         dataset_files_pattern,
-        # split_dataset_files=False,
         opt=False,
         ignore_hydrogens=False,
         num_conformers=10,
@@ -62,6 +58,7 @@ class GetSimilarityScores:
             num_conformers=0,
         )
         self.ref_mol = ref_mol[0]
+        # TODO:Check if saving all molecules into numpy arrays will cause memory leaks
         self.dataset_mols, self.dataset_names = prepare_mols(
             self.dataset_files,
             opt=opt,
@@ -69,26 +66,6 @@ class GetSimilarityScores:
             num_conformers=num_conformers,
             random_seed=random_seed,
         )
-
-        # TODO: this only works if the input is an sdf file, what about other mol format?
-        # if split_dataset_files:
-        #     assert len(self.dataset_files) == 1
-        #     self.dataset_files = split_sdf_file(
-        #         self.dataset_files[0],
-        #         output_dir=self.working_dir,
-        #         max_mols_per_file=1,
-        #         ignore_hydrogens=ignore_hydrogens,
-        #         cleanup=False,
-        #     )
-
-        # TODO:Check if saving all molecules into numpy arrays will cause memory leaks
-        # self.ref_mol = self._process_molecule(
-        #     self.ref_file, opt=opt, ignore_hydrogens=ignore_hydrogens
-        # )
-        # self.dataset_mols = [
-        #     self._process_molecule(file, opt=opt, ignore_hydrogens=ignore_hydrogens)
-        #     for file in self.dataset_files
-        # ]
 
         self.transformation_arrays = None
         self.rotation = np.array([])
@@ -121,15 +98,11 @@ class GetSimilarityScores:
                 else t.reshape(1, 3)
             )
 
-    def transform_molecules(self, write_to_file=False, filename="pypaper.sdf"):
+    def transform_molecules(self):
         for mol, rot, trans in zip(self.dataset_mols, self.rotation, self.translation):
             xyz_trans = mol.transform_mol(rot, trans)
             mol.create_molecule(xyz_trans)
             self.transformed_molecules.append(mol)
-        # if write_to_file:
-        #     sd_writer = AllChem.SDWriter(f"{self.working_dir}/{filename}")
-        #     for mol in [self.ref_mol] + self.transformed_molecules:
-        #         sd_writer.write(mol.mol)
 
     def calculate_tanimoto(
         self,
@@ -155,14 +128,12 @@ class GetSimilarityScores:
                 epsilon=epsilon,
                 use_carbon_radii=use_carbon_radii,
             )
-            # ref_overlap = calc_analytic_overlap_vol(self.ref_mol, self.ref_mol)
             inputs = [
                 (self.ref_mol, fit_mol, n, proxy_cutoff, epsilon, use_carbon_radii)
                 for fit_mol in self.transformed_molecules
             ]
 
             with Pool(processes=cpu_count()) as pool:
-                # outputs = pool.starmap(calc_multi_analytic_overlap_vol, inputs)
                 outputs = pool.starmap(
                     calc_multi_analytic_overlap_vol_recursive, inputs
                 )
@@ -189,7 +160,6 @@ class GetSimilarityScores:
             )
 
         if color:
-            # color_ts = []
             ref_pharm = calc_pharmacophore(self.ref_mol.mol)
             ref_volume = calc_pharm_overlap(ref_pharm, ref_pharm)
             inputs = [(fit_mol, ref_pharm) for fit_mol in self.transformed_molecules]
@@ -219,16 +189,7 @@ class GetSimilarityScores:
                 alpha=0.95,
                 beta=0.05,
             )
-
-            # for fit_mol in self.transformed_molecules:
-            #     fit_pharm = calc_pharmacophore(fit_mol.mol)
-            #     fit_volume = calc_full_pharm_overlap(fit_pharm, fit_pharm)
-            #     ref_fit_volume = calc_full_pharm_overlap(ref_pharm, fit_pharm)
-            #
-            #     t = color_tanimoto(self.ref_mol.mol, fit_mol.mol)
-            #     color_ts.append(t)
         else:
-            # color_ts = None
             color_tanimoto, color_fit_tversky, color_ref_tversky = [
                 np.zeros(len(self.transformed_molecules))
             ] * 3
@@ -237,9 +198,7 @@ class GetSimilarityScores:
         full_fit_overlap = outputs[:, 0]
         full_ref_fit_overlap = outputs[:, 1]
         full_ref_overlap = np.ones_like(full_fit_overlap) * ref_overlap
-        # full_tanimoto = full_ref_fit_overlap / (
-        #     full_ref_overlap + full_fit_overlap - full_ref_fit_overlap
-        # )
+
         shape_tanimoto = calc_tanimoto(
             full_ref_overlap, full_fit_overlap, full_ref_fit_overlap
         )
@@ -260,9 +219,6 @@ class GetSimilarityScores:
 
         df_data = {
             "Molecule": self.dataset_names,
-            # "Molecule": [
-            #     os.path.basename(path).split(".")[0] for path in self.dataset_files
-            # ],
             "ComboTanimoto": shape_tanimoto + color_tanimoto,
             "ShapeTanimoto": shape_tanimoto,
             "ColorTanimoto": color_tanimoto,
@@ -274,15 +230,15 @@ class GetSimilarityScores:
             "RefColorTversky": color_ref_tversky,
             "Overlap": full_ref_fit_overlap,
         }
-        # if color_tanimoto is not None:
-        #     df_data["ColorTanimoto"] = color_tanimoto
-        #     df_data["ComboTanimoto"] = df_data["ShapeTanimoto"] + df_data["ColorTanimoto"]
 
         df = pd.DataFrame(df_data)
         df["Prefix"] = df["Molecule"].str.split("_").str[0]
         df = df.sort_values(by=["Prefix", sort_by], ascending=[True, False])
-        idx = df.groupby("Prefix").apply(lambda x: x.nlargest(max_conformers, sort_by)).index.levels[
-            1]
+        idx = (
+            df.groupby("Prefix")
+            .apply(lambda x: x.nlargest(max_conformers, sort_by))
+            .index.levels[1]
+        )
         df = df.loc[idx].sort_values(by=sort_by, ascending=False).round(3)
         del df["Prefix"]
         df.to_csv(f"{self.working_dir}/pypaper.csv", index=False, sep="\t")
@@ -305,5 +261,4 @@ class GetSimilarityScores:
                         mol_prop = str(mol_props[col])
                         mol.mol.SetProp("PYPAPER_" + col, mol_prop)
                 sd_writer.write(mol.mol)
-
         return df
