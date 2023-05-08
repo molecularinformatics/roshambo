443032911f0cf5c9f5413062811d45830c6af934
ratwi <rasha.atwi@biogen.com>
2023-05-08 10:02:07 -0400

    Add partial documentation of analytic_overlap

--- roshambo/analytic_overlap.py
+++ roshambo/analytic_overlap.py
@@ -9,15 +9,35 @@ from roshambo import constants
 
 
 def calc_analytic_overlap_vol(ref_mol, fit_mol):
+    """
+    Calculates the Gaussian overlap volume between two molecules using an analytic
+    method. Please refer to Eq. (11) and (12) of this paper by Grant et. al.:
+    https://doi.org/10.1002/(SICI)1096-987X(19961115)17:14%3C1653::AID-JCC7%3E3.0.CO;2-K.
+
+    Args:
+        ref_mol (Molecule):
+            The reference molecule object.
+        fit_mol (Molecule):
+            The fit molecule object.
+
+    Returns:
+        float:
+            The analytic overlap volume between the two molecules.
+    """
+
+    # Get the atomic coordinates and radii for each molecule
     ref_mol_coords_radii = ref_mol.get_atomic_coordinates_and_radii()
     fit_mol_coords_radii = fit_mol.get_atomic_coordinates_and_radii()
 
+    # Get the atomic coordinates for each molecule
     ref_coords = ref_mol_coords_radii[:, :3]
     fit_coords = fit_mol_coords_radii[:, :3]
 
+    # Get the atomic radii for each molecule
     ref_radii = ref_mol_coords_radii[:, 3]
     fit_radii = fit_mol_coords_radii[:, 3]
 
+    # Calculate the distance squared between each pair of atoms
     dist_sqr = np.sum(
         (ref_coords[:, np.newaxis, :] - fit_coords[np.newaxis, :, :]) ** 2, axis=2
     )
@@ -28,21 +48,67 @@ def calc_analytic_overlap_vol(ref_mol, fit_mol):
     aj = (constants.KAPPA / (fit_radii**2))[np.newaxis, :]
     aij = ai + aj
     dij = ai * aj * dist_sqr
+
+    # Calculate the Gaussian overlap volume for each pair of atoms
     vij = pij * np.exp(-dij / aij) * (constants.PI / aij) ** 1.5
+
+    # Sum up the Gaussian overlap volumes to get the total overlap volume
     overlap = np.sum(vij)
     return overlap
 
 
 def calc_multi_analytic_overlap_vol(ref_mol, fit_mol):
+    """
+    Calculates the self-overlap volume of the fit molecule and the overlap volume
+    between a reference molecule and a fit molecule using the calc_analytic_overlap_vol
+    function.
+
+    Args:
+        ref_mol (Molecule):
+            The reference molecule object.
+        fit_mol (Molecule):
+            The fit molecule object.
+
+    Returns:
+        Tuple[float, float]:
+            A tuple containing the following:
+            - Fit overlap: The self overlap volume of the fit molecule.
+            - Reference fit overlap: The overlap volume between the reference molecule
+            and the fit molecule.
+    """
     fit_overlap = calc_analytic_overlap_vol(fit_mol, fit_mol)
     ref_fit_overlap = calc_analytic_overlap_vol(ref_mol, fit_mol)
     return fit_overlap, ref_fit_overlap
 
 
 def calc_single_overlap(atom_inds, alpha_dict, cross_alpha_distance_dict):
+    """
+    Uses the Gaussian product theorem to calculate the product of n Gaussians, as a
+    single Gaussian function.
+
+    Args:
+        atom_inds (list):
+            List of atomic indices for which to calculate the single Gaussian function.
+        alpha_dict (dict):
+            Dictionary mapping atomic indices to alpha values (KAPPA/radius**2).
+        cross_alpha_distance_dict (dict):
+            Dictionary of dictionaries of this form:
+            {i: {j: alpha_i * alpha_j * (dist_ij)**2, k: ...}, ...}, where
+            i, j, k, ... are atomic indices.
+
+    Returns:
+        float:
+            The calculated Gaussian function.
+    """
+
+    # Calculate the product of the Gaussian weight factors
     p = constants.CONSTANT_P ** (len(atom_inds))
+
+    # Calculate the total exponent as the sum of the alpha values
     alpha = sum([alpha_dict[i] for i in atom_inds])
+
     k = [cross_alpha_distance_dict[i][j] for i, j in combinations(atom_inds, 2)]
+    # Calculate the exponent term and return the final overlap value
     k_exp = constants.EXP ** (-sum(k) / alpha)
     return p * k_exp * (constants.PI / alpha) ** 1.5
 
@@ -57,6 +123,31 @@ def _calc_overlap(
     cross_alpha_distance_dict,
     cross_distance_bool_dict,
 ):
+    """
+    Calculates the overlap between two molecules based on the gaussian product theorem.
+
+    Args:
+        atom_inds (list):
+            List of atom indices for which the overlap needs to be calculated.
+        rem_ind (set):
+            Set of atom indices which should be skipped.
+        len_full (int):
+            Number of atoms in the fit molecule.
+        len_ref (int):
+            Number of atoms in the reference molecule.
+        n (int):
+            The order of the overlap calculation.
+        alpha_dict (dict):
+            Dictionary containing alpha values for each atom index.
+        cross_alpha_distance_dict (dict):
+            Dictionary containing pairwise alpha distance values.
+        cross_distance_bool_dict (dict):
+            Dictionary containing boolean values for pairwise distance values between
+            the reference and fit molecules.
+
+    Returns:
+        float: The overlap between the two molecules.
+    """
     if n == 1:
         return calc_single_overlap(atom_inds, alpha_dict, cross_alpha_distance_dict)
     else:
