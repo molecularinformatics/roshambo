00fe8734c9d1c0fdc363a6f76d83cd5ea297beb3
ratwi <rasha.atwi@biogen.com>
2023-05-15 10:31:52 -0400

    Update the way pharmacophores are draw, e.g. add legend, change color, etc.

--- roshambo/smarts.py
+++ roshambo/smarts.py
@@ -2,7 +2,10 @@ import os
 import json
 
 import numpy as np
+import matplotlib.image as img
+import matplotlib.pyplot as plt
 
+from cairosvg import svg2png
 from IPython.display import SVG
 from collections import defaultdict
 
@@ -69,33 +72,25 @@ def calc_custom_pharm(rdkit_mol, compiled_smarts):
     return pharmacophore
 
 
-def draw_pharm(rdkit_mol, feats, filename):
-    colors = {
-        "Donor": (1, 0.7451, 0.0431),
-        "Acceptor": (0.9843, 0.3373, 0.0275),
-        "PosIonizable": (1, 0, 0.4314),
-        "NegIonizable": (0.5137, 0.2196, 0.9255),
-        "Aromatic": (0.2275, 0.5255, 1),
-        "Hydrophobe": (1, 0, 1),
-    }
+def draw_pharm(rdkit_mol, features, filename="pharm.jpg"):
     atom_highlights = defaultdict(list)
     highlight_rads = {}
-    for feature_type, features in feats.items():
-        if feature_type in colors:
-            clr = colors[feature_type]
-            for aid in features:
-                for atom in aid[0]:
-                    atom_highlights[atom].append(clr)
-                    highlight_rads[atom] = 0.5
+    for feature in features:
+        if feature[0] in FEATURES:
+            color = FEATURES[feature[0]][2]
+            for atom_id in feature[1]:
+                atom_highlights[atom_id].append(color)
+                highlight_rads[atom_id] = 0.5
 
     rdDepictor.Compute2DCoords(rdkit_mol)
     rdDepictor.SetPreferCoordGen(True)
-    drawer = rdMolDraw2D.MolDraw2DSVG(300, 300)
+    drawer = rdMolDraw2D.MolDraw2DSVG(800, 800)
+    # Use black for all elements
     drawer.drawOptions().updateAtomPalette(
         {k: (0, 0, 0) for k in DrawingOptions.elemDict.keys()}
     )
     drawer.SetLineWidth(2)
-    drawer.SetFontSize(1.0)
+    drawer.SetFontSize(6.0)
     drawer.drawOptions().continuousHighlight = False
     drawer.drawOptions().splitBonds = False
     drawer.drawOptions().fillHighlights = True
@@ -108,5 +103,56 @@ def draw_pharm(rdkit_mol, feats, filename):
     drawer.FinishDrawing()
     svg = drawer.GetDrawingText().replace("svg:", "")
     SVG(svg)
-    with open(filename, "w") as f:
+    with open("pharm.svg", "w") as f:
         f.write(svg)
+
+    svg2png(bytestring=svg, write_to="image.png")
+
+    fig, (ax, picture) = plt.subplots(
+        nrows=2,
+        figsize=(4, 4),
+        gridspec_kw={"height_ratios": [1, 5]},
+    )
+
+    mol_image = img.imread("image.png")
+    picture.imshow(mol_image)
+    picture.axis("off")
+    os.remove("image.png")
+    os.remove("pharm.svg")
+
+    # Data for the circles
+    circle_radii = [0, 50, 100, 150, 200, 250]
+    feature_values = list(FEATURES.values())
+    circle_colors = [i[2] for i in feature_values]
+    circle_annotations = [
+        "Donor",
+        "Acceptor",
+        "Cation",
+        "Anion",
+        "Ring",
+        "Hydrophobe",
+    ]
+    # Draw the circles and annotations
+    for radius, color, annotation in zip(
+        circle_radii, circle_colors, circle_annotations
+    ):
+        x = radius
+        circle = plt.Circle((x, -5), 5, color=color)  # , alpha=0.5)
+        ax.add_patch(circle)
+        ax.annotate(
+            annotation,
+            (x, 10),
+            va="center",
+            ha="center",
+            fontsize=6,
+            fontweight="bold",
+        )
+
+    # Set axis limits
+    ax.set_xlim(-10, 270)
+    ax.set_ylim(-20, 20)
+    ax.axis("off")
+
+    # Set aspect ratio to equal
+    ax.set_aspect("equal", adjustable="box")
+    plt.savefig(filename, dpi=600)
